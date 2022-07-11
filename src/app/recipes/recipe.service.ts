92f256a6b63b8e13ee5b7a3ce544982458cc2f17
Nicelx <relush321@gmail.com>
2022-07-11 23:57:46 +0300

    adding ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,32 @@
 import { Recipe } from "./recipe.model";
-import { EventEmitter } from '@angular/core';
+import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
 	recipeSelected = new EventEmitter<Recipe>();
-	private recipes : Recipe[]= [
-		new Recipe('First recipe', 'first description', 'https://live.staticflickr.com/65535/51129079431_9ec756feb6_b.jpg'),
-		new Recipe('second recipe', 'second description', 'https://live.staticflickr.com/65535/51129079431_9ec756feb6_b.jpg')
-	  ];
+	private recipes: Recipe[] = [
+		new Recipe(
+			"Окрошка",
+			"Отличный вкус лета",
+			"https://eda.ru/img/eda/c1200x1280/s1.eda.ru/StaticContent/Photos/140424040926/140429194401/p_O.jpg",
+			[
+				new Ingredient("квас", 1),
+				new Ingredient("огурец", 1),
+				new Ingredient("картофель", 2-3),
+				new Ingredient("зелень", 1),
+				new Ingredient("сметана", 1),
+			]
+		),
+		new Recipe(
+			"Жареная картшка",
+			"Батька одобряет",
+			"https://s1.eda.ru/StaticContent/Photos/170306185211/210216083616/p_O.jpg",
+			[
+				new Ingredient('картофель' ,3),
+				new Ingredient('масло' ,1),
+			]
+		),
+	];
 
 	getRecipes() {
 		return this.recipes.slice();
