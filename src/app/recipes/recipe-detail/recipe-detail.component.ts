daa05521443031e97b1846f67591a603fc6aa7cd
Nicelx <relush321@gmail.com>
2022-07-25 22:08:01 +0300

    routing

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -12,7 +12,7 @@ export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
   id: number;
 
-  constructor(private recipeService : RecipeService, private route: ActivatedRoute) { }
+  constructor(private recipeService : RecipeService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(
@@ -25,5 +25,10 @@ export class RecipeDetailComponent implements OnInit {
 
   onAddToShoppingList() {
     // this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    // this.router.navigate(['edit'], {relativeTo: this.route} )
+    this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route})
   }
 }
