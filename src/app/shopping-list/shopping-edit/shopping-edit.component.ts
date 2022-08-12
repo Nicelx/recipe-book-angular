3864278a403b8206a0aa6ad2b334d83359ccc369
Nicelx <relush321@gmail.com>
2022-08-12 23:54:11 +0300

    reset form, clear, delete

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -32,7 +32,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     );
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const {value} = form;
     const newIngredient = new Ingredient(value.name, value.amount);
     if (this.editMode) {
@@ -40,7 +40,20 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     } else {
       this.slService.addIngredient(newIngredient);
     }
+    this.editMode = false;
+    form.reset();
   }
+
+  onClear() {
+    this.slForm.reset()
+    this.editMode = false;
+  }
+
+  onDelete() {
+    this.slService.deleteIngredient(this.editedItemIndex);
+    this.onClear();
+  }
+
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
