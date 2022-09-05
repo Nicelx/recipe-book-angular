4ae899b2b5abafad43eed18360587c5ddd1809d2
Nicelx <relush321@gmail.com>
2022-09-05 23:50:31 +0300

    fetching recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
 
 @Injectable({
 	providedIn: "root",
@@ -9,14 +10,23 @@ export class DataStorageService {
 	constructor(private http: HttpClient, private recipeService: RecipeService) {}
 	storeRecipes() {
 		const recipes = this.recipeService.getRecipes();
-		1;
 		this.http
 			.put(
 				"https://angular-test-afd6e-default-rtdb.europe-west1.firebasedatabase.app/recipes.json",
 				recipes
 			)
-			.subscribe(response => {
+			.subscribe((response) => {
 				console.log(response);
+			});
+	}
+
+	fetchRecipes() {
+		this.http
+			.get<Recipe[]>(
+				"https://angular-test-afd6e-default-rtdb.europe-west1.firebasedatabase.app/recipes.json"
+			)
+			.subscribe((recipes) => {
+				this.recipeService.setRecipes(recipes);
 			});
 	}
 }
