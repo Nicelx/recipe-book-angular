fcea12d786acb3e3dc62bd25ef90f0c8a7a517a6
Nicelx <relush321@gmail.com>
2022-09-06 23:44:55 +0300

    transforming response data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,29 +9,31 @@ export class RecipeService {
 	recipesChanged = new Subject<Recipe[]>();
 
 
-	private recipes: Recipe[] = [
-		new Recipe(
-			"Окрошка",
-			"Отличный вкус лета",
-			"https://eda.ru/img/eda/c1200x1280/s1.eda.ru/StaticContent/Photos/140424040926/140429194401/p_O.jpg",
-			[
-				new Ingredient("квас", 1),
-				new Ingredient("огурец", 1),
-				new Ingredient("картофель", 2-3),
-				new Ingredient("зелень", 1),
-				new Ingredient("сметана", 1),
-			]
-		),
-		new Recipe(
-			"Жареная картшка",
-			"Батька одобряет",
-			"https://s1.eda.ru/StaticContent/Photos/170306185211/210216083616/p_O.jpg",
-			[
-				new Ingredient('картофель' ,3),
-				new Ingredient('масло' ,1),
-			]
-		),
-	];
+	// private recipes: Recipe[] = [
+	// 	new Recipe(
+	// 		"Окрошка",
+	// 		"Отличный вкус лета",
+	// 		"https://eda.ru/img/eda/c1200x1280/s1.eda.ru/StaticContent/Photos/140424040926/140429194401/p_O.jpg",
+	// 		[
+	// 			new Ingredient("квас", 1),
+	// 			new Ingredient("огурец", 1),
+	// 			new Ingredient("картофель", 2-3),
+	// 			new Ingredient("зелень", 1),
+	// 			new Ingredient("сметана", 1),
+	// 		]
+	// 	),
+	// 	new Recipe(
+	// 		"Жареная картшка",
+	// 		"Батька одобряет",
+	// 		"https://s1.eda.ru/StaticContent/Photos/170306185211/210216083616/p_O.jpg",
+	// 		[
+	// 			new Ingredient('картофель' ,3),
+	// 			new Ingredient('масло' ,1),
+	// 		]
+	// 	),
+	// ];
+
+	private recipes: Recipe[] = [];
 
 	constructor(private slService: ShoppingListService) {
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
+import { map } from "rxjs/operators";
 
 @Injectable({
 	providedIn: "root",
@@ -25,6 +26,11 @@ export class DataStorageService {
 			.get<Recipe[]>(
 				"https://angular-test-afd6e-default-rtdb.europe-west1.firebasedatabase.app/recipes.json"
 			)
+			.pipe(map(recipes => {
+				return recipes.map(recipe => {
+					return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+				})
+			}))
 			.subscribe((recipes) => {
 				this.recipeService.setRecipes(recipes);
 			});
