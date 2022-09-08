2d30ef66fc986e9c7956cca1e786525419d2d22c
Nicelx <relush321@gmail.com>
2022-09-08 23:47:16 +0300

    finish http part

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from "@angular/core";
-import {  ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
-import { resolve } from "dns";
-import { DataStorageService } from "../shared/data-storage.service";
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
 import { Recipe } from "./recipe.model";
+import { DataStorageService } from "../shared/data-storage.service";
+import { RecipeService } from './recipe.service';
 
-@Injectable({providedIn: 'root'})
-export class RecipesResolverService implements Resolve<Recipe[]>{
-	constructor(private dataStorageService: DataStorageService) {
-		resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-			
+@Injectable({ providedIn: "root" })
+export class RecipesResolverService implements Resolve<Recipe[]> {
+	constructor(private dataStorageService: DataStorageService, private recipesService: RecipeService) {}
+	resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+		const recipes = this.recipesService.getRecipes()
+
+		if (recipes.length === 0) {
+			return this.dataStorageService.fetchRecipes();
+		} else {
+			return recipes;
 		}
 	}
-} 
+}
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
-import { map } from "rxjs/operators";
+import { map, tap } from "rxjs/operators";
 
 @Injectable({
 	providedIn: "root",
@@ -22,17 +22,22 @@ export class DataStorageService {
 	}
 
 	fetchRecipes() {
-		this.http
+		return this.http
 			.get<Recipe[]>(
 				"https://angular-test-afd6e-default-rtdb.europe-west1.firebasedatabase.app/recipes.json"
 			)
-			.pipe(map(recipes => {
-				return recipes.map(recipe => {
-					return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+			.pipe(
+				map((recipes) => {
+					return recipes.map((recipe) => {
+						return {
+							...recipe,
+							ingredients: recipe.ingredients ? recipe.ingredients : [],
+						};
+					});
+				}),
+				tap((recipes) => {
+					this.recipeService.setRecipes(recipes);
 				})
-			}))
-			.subscribe((recipes) => {
-				this.recipeService.setRecipes(recipes);
-			});
+			);
 	}
 }
