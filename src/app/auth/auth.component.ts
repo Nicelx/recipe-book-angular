ec8bb969825275052739a8c646dd2e900aceac4c
Nicelx <relush321@gmail.com>
2022-09-25 23:53:12 +0300

    creating component programmatically

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,18 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, ComponentFactoryResolver, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { Observable } from "rxjs";
 import { AuthResponseData, AuthService } from "./auth.service";
 import { Router } from '@angular/router';
+import { AlertComponent } from "../shared/alert/alert.component";
+import { PlaceholderDirective } from "../shared/placeholder/placeholder.directive";
+
 
 @Component({
 	selector: "app-auth",
 	templateUrl: "./auth.component.html",
 })
 export class AuthComponent {
-	constructor(private authService: AuthService, private router : Router) {}
+	constructor(private authService: AuthService, private router : Router, private componentFactoryResolver: ComponentFactoryResolver) {}
 	isLoginMode = true;
 	isLoading = false;
 	error: string = null;
+	@ViewChild(PlaceholderDirective, {static: false}) alertHost: PlaceholderDirective;
 
 	onSwitchMode() {
 		this.isLoginMode = !this.isLoginMode;
@@ -46,6 +50,7 @@ export class AuthComponent {
 				this.isLoading = false;
 
 				this.error = errorMessage;
+				this.showErrorAlert(errorMessage);
 				console.log(errorMessage);
 			}
 		);
@@ -54,5 +59,13 @@ export class AuthComponent {
 
 	onHandleError() {
 		this.error = null;
+	}
+
+	private showErrorAlert(message: string) {
+		const alertCmp = new AlertComponent();
+		const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent)
+		const hostViewContainerRef = this.alertHost.viewContainerRef;
+		hostViewContainerRef.clear();
+		hostViewContainerRef.createComponent(alertCmpFactory);
 	}
 }
