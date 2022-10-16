fd907f562ac80753a78a3f3f70ebdf357dc91005
Nicelx <relush321@gmail.com>
2022-10-16 23:13:03 +0300

    update, delete ingredients with ngrx

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -4,7 +4,7 @@ import { Ingredient } from "src/app/shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list.service";
 import { Subscription } from "rxjs";
 import { Store } from "@ngrx/store";
-import * as ShoppingListActions from '../store/shopping-list.actions';
+import * as ShoppingListActions from "../store/shopping-list.actions";
 
 @Component({
 	selector: "app-shopping-edit",
@@ -39,10 +39,14 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
 		const { value } = form;
 		const newIngredient = new Ingredient(value.name, value.amount);
 		if (this.editMode) {
-			this.slService.updateIngredient(this.editedItemIndex, newIngredient);
+			this.store.dispatch(
+				new ShoppingListActions.UpdateIngredient({
+					index: this.editedItemIndex,
+					ingredient: newIngredient,
+				})
+			);
 		} else {
-			// this.slService.addIngredient(newIngredient);
-      this.store.dispatch(new ShoppingListActions.AddIngredient(newIngredient))
+			this.store.dispatch(new ShoppingListActions.AddIngredient(newIngredient));
 		}
 		this.editMode = false;
 		form.reset();
@@ -54,7 +58,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
 	}
 
 	onDelete() {
-		this.slService.deleteIngredient(this.editedItemIndex);
+		// this.slService.deleteIngredient(this.editedItemIndex);
+		this.store.dispatch(new ShoppingListActions.DeleteIngredient(this.editedItemIndex))
 		this.onClear();
 	}
 
