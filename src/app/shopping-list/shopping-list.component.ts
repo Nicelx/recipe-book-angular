9d86032284b0191831ba5139c62e6e819c81c75c
Nicelx <relush321@gmail.com>
2022-10-18 23:57:19 +0300

    start & stop edit actions and reducers

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -7,6 +7,7 @@ import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "./shopping-list.service";
 
 import * as fromShoppingList from './store/shopping-list.reducer'
+import * as ShoppingListActions from './store/shopping-list.actions'
 
 @Component({
 	selector: "app-shopping-list",
@@ -38,6 +39,7 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
 		// this.igChangeSub.unsubscribe();
 	}
 	onEditItem(index: number) {
-		this.slService.startedEditing.next(index);
+		// this.slService.startedEditing.next(index);
+		this.store.dispatch(new ShoppingListActions.StartEdit(index))
 	}
 }
