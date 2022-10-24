aa9c1e0264fc4b1ec496d854bce9b174b9bd70c2
Nicelx <relush321@gmail.com>
2022-10-24 23:52:46 +0300

    fix bug, prefixes to actions

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -12,7 +12,7 @@ export class AuthInterceptorService implements HttpInterceptor {
 	constructor(private authService: AuthService, private store: Store<fromApp.AppState>) {}
 
 	intercept(req: HttpRequest<any>, next: HttpHandler) {
-		return this.store.select("auth").user.pipe(
+		return this.store.select("auth").pipe(
 			take(1),
 			map(authState => {
 				return authState.user
