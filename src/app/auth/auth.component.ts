5cb3d2bf1f57e79348c6f8d6f0d2b14a90e9fde6
Nicelx <relush321@gmail.com>
2022-10-28 23:41:48 +0300

    managing ui state in ngrx

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,4 @@
-import { Component, ComponentFactoryResolver, OnDestroy, ViewChild } from "@angular/core";
+import { Component, ComponentFactoryResolver, OnDestroy, OnInit, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { Observable, Subscription } from "rxjs";
 import { Router } from "@angular/router";
@@ -7,14 +7,14 @@ import { Store } from "@ngrx/store";
 import { AuthResponseData, AuthService } from "./auth.service";
 import { AlertComponent } from "../shared/alert/alert.component";
 import { PlaceholderDirective } from "../shared/placeholder/placeholder.directive";
-import * as fromApp from '../store/app.reducer';
-import * as AuthActions from './store/auth.actions'
+import * as fromApp from "../store/app.reducer";
+import * as AuthActions from "./store/auth.actions";
 
 @Component({
 	selector: "app-auth",
 	templateUrl: "./auth.component.html",
 })
-export class AuthComponent implements OnDestroy {
+export class AuthComponent implements OnInit, OnDestroy {
 	constructor(
 		private authService: AuthService,
 		private router: Router,
@@ -27,6 +27,13 @@ export class AuthComponent implements OnDestroy {
 	@ViewChild(PlaceholderDirective, { static: false }) alertHost: PlaceholderDirective;
 
 	private closeSub: Subscription;
+
+	ngOnInit() {
+		this.store.select("auth").subscribe((authState) => {
+			this.isLoading = authState.loading;
+			this.error = authState.authError;
+		});
+	}
 
 	onSwitchMode() {
 		this.isLoginMode = !this.isLoginMode;
@@ -46,28 +53,30 @@ export class AuthComponent implements OnDestroy {
 
 		if (this.isLoginMode) {
 			// authObs = this.authService.login(email, password);
-			this.store.dispatch(new AuthActions.LoginStart({
-				email,
-				password
-			}))
+			this.store.dispatch(
+				new AuthActions.LoginStart({
+					email,
+					password,
+				})
+			);
 		} else {
 			authObs = this.authService.signup(email, password);
 		}
 
-		authObs.subscribe(
-			(resData) => {
-				this.isLoading = false;
-				this.router.navigate(["/recipes"]);
-				console.log("res data", resData);
-			},
-			(errorMessage) => {
-				this.isLoading = false;
+		// authObs.subscribe(
+		// 	(resData) => {
+		// 		this.isLoading = false;
+		// 		this.router.navigate(["/recipes"]);
+		// 		console.log("res data", resData);
+		// 	},
+		// 	(errorMessage) => {
+		// 		this.isLoading = false;
 
-				this.error = errorMessage;
-				this.showErrorAlert(errorMessage);
-				console.log(errorMessage);
-			}
-		);
+		// 		this.error = errorMessage;
+		// 		this.showErrorAlert(errorMessage);
+		// 		console.log(errorMessage);
+		// 	}
+		// );
 		form.reset();
 	}
 
--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -4,6 +4,7 @@ import { Action } from "@ngrx/store";
 
 export const LOGIN_START = '[Auth] Login Start'
 export const LOGIN = "[Auth] Login";
+export const LOGIN_FAIL = "[Auth] Login Fail"
 export const LOGOUT = "[Auth] Logout";
 
 export class Login implements Action {
@@ -26,5 +27,13 @@ export class LoginStart implements Action {
 	}
 }
 
+export class LoginFail implements Action {
+	readonly type = LOGIN_FAIL
 
-export type AuthActions = Login | Logout;
+	constructor(public payload: string) {
+
+	}
+}
+
+
+export type AuthActions = Login | Logout| LoginStart | LoginFail;
