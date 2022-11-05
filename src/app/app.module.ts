3c54aa408c9939a121686167bf1c181118cbb8f6
Nicelx <relush321@gmail.com>
2022-11-05 23:49:28 +0300

    store devtools and router store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from "@angular/platform-browser";
 import { HttpClientModule } from "@angular/common/http";
 import { StoreModule } from "@ngrx/store";
 import { EffectsModule } from "@ngrx/effects";
+import {StoreDevtoolsModule} from "@ngrx/store-devtools"
+import {StoreRouterConnectingModule} from '@ngrx/router-store';
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
@@ -11,15 +13,20 @@ import { SharedModule } from "./shared/shared.module";
 import { CoreModule } from "./core.module";
 import * as fromApp from './store/app.reducer'
 import { AuthEffects } from "./auth/store/auth.effects";
+import { environment } from '../environments/environment';
 
 @NgModule({
 	declarations: [AppComponent, HeaderComponent],
 	imports: [
-		BrowserModule,
+	BrowserModule,
 		AppRoutingModule,
 		HttpClientModule,
 		StoreModule.forRoot(fromApp.appReducer),
 		EffectsModule.forRoot([AuthEffects]),
+		StoreDevtoolsModule.instrument({
+			logOnly: environment.production
+		}),
+		StoreRouterConnectingModule.forRoot(),
 		SharedModule,
 		CoreModule,
 	],
