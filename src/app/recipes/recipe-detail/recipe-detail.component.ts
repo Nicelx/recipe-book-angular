19c006935f1dec626b973fdc972fd38e14cd4c96
Nicelx <relush321@gmail.com>
2022-11-07 23:43:37 +0300

    fetching recipe detail data

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,38 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router } from '@angular/router';
-import { RecipeService } from '../recipe.service';
-import { Recipe } from './../recipe.model';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Params, Router } from "@angular/router";
+import { Store } from "@ngrx/store";
+
+import { RecipeService } from "../recipe.service";
+import { Recipe } from "./../recipe.model";
+import * as fromApp from "../../store/app.reducer";
+import { map, switchMap } from "rxjs/operators";
 
 @Component({
-  selector: 'app-recipe-detail',
-  templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+	selector: "app-recipe-detail",
+	templateUrl: "./recipe-detail.component.html",
+	styleUrls: ["./recipe-detail.component.css"],
 })
 export class RecipeDetailComponent implements OnInit {
-  recipe: Recipe;
-  id: number;
+	recipe: Recipe;
+	id: number;
 
-  constructor(private recipeService : RecipeService, private route: ActivatedRoute, private router: Router) { }
+	constructor(
+		private recipeService: RecipeService,
+		private route: ActivatedRoute,
+		private router: Router,
+		private store: Store<fromApp.AppState>
+	) {}
 
-  ngOnInit(): void {
-    this.route.params.subscribe(
-      (params: Params) => {
-        this.id = +params['id']
-        this.recipe = this.recipeService.getRecipe(this.id);
-      }
-    )
-  }
+	ngOnInit(): void {
+		this.route.params
+			.pipe(
+				map((params) => {
+					return +params["id"];
+				}),
+				switchMap((id) => {
+					this.id = id;
+					return this.store.select("recipes");
+				}),
+				map((recipesState) => {
+					return recipesState.recipes.find((recipe, index) => {
+						return index === this.id;
+					});
+				})
+			)
+			.subscribe((recipe) => {
+				this.recipe = recipe;
+			});
+	}
 
-  onAddToShoppingList() {
-    // this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
-  }
+	onAddToShoppingList() {
+		// this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+	}
 
-  onEditRecipe() {
-    this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route})
-  }
+	onEditRecipe() {
+		this.router.navigate(["../", this.id, "edit"], { relativeTo: this.route });
+	}
 
-  onDeleteRecipe() {
-    this.recipeService.deleteRecipe(this.id);
-    this.router.navigate(['/recipes'])
-  }
+	onDeleteRecipe() {
+		this.recipeService.deleteRecipe(this.id);
+		this.router.navigate(["/recipes"]);
+	}
 }
