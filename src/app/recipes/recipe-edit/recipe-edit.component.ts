35c4dba9cfe0fe691318fa10ef3fd9d9f3727624
Nicelx <relush321@gmail.com>
2022-11-12 23:55:16 +0300

    dispatching actions

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from "@angular/core";
 import { FormArray, FormControl, FormGroup, Validators } from "@angular/forms";
 import { ActivatedRoute, Params, Router } from "@angular/router";
 import { Store } from "@ngrx/store";
+import { map } from "rxjs/operators";
 
 import { RecipeService } from "./../recipe.service";
 import * as fromApp from "../../store/app.reducer";
-import { map } from "rxjs/operators";
+import * as RecipesActions from '../store/recipe.actions';
+import { UpdateRecipe } from './../store/recipe.actions';
 
 @Component({
 	selector: "app-recipe-edit",
@@ -34,9 +36,14 @@ export class RecipeEditComponent implements OnInit {
 
 	onSubmit() {
 		if (this.editMode) {
-			this.recipeService.updateRecipe(this.id, this.recipeForm.value);
+			// this.recipeService.updateRecipe(this.id, this.recipeForm.value);
+			this.store.dispatch(new RecipesActions.UpdateRecipe({
+				index: this.id,
+				newRecipe: this.recipeForm.value
+			}))
 		} else {
-			this.recipeService.addRecipe(this.recipeForm.value);
+			// this.recipeService.addRecipe(this.recipeForm.value);
+			this.store.dispatch(new RecipesActions.AddRecipe(this.recipeForm.value))
 		}
 		this.onCancel();
 	}
