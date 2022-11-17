b2a6a5355e937d25d1afcd0ddf8a34198779d175
Nicelx <relush321@gmail.com>
2022-11-17 23:56:28 +0300

    pain...

--- src/app/recipes/store/recipe.reducer.ts
+++ src/app/recipes/store/recipe.reducer.ts
@@ -10,42 +10,44 @@ const initialState: State = {
 };
 
 export function recipeReducer(state = initialState, action: RecipesActions.RecipeActions) {
-	switch (action.type) {
-		case RecipesActions.SET_RECIPES:
-			return {
-				...state,
-				recipes: [...action.payload],
-			};
-		case RecipesActions.ADD_RECIPE:
-			return {
-				...state,
-				recipes: [...state.recipes, action.payload],
-			};
+	try {
+		switch (action.type) {
+			case RecipesActions.SET_RECIPES:
+				return {
+					...state,
+					recipes: [...action.payload],
+				};
+			case RecipesActions.ADD_RECIPE:
+				return {
+					...state,
+					recipes: [...state.recipes, action.payload],
+				};
 
-		case RecipesActions.UPDATE_RECIPE:
-			const updatedRecipe = {
-				...state.recipes[action.payload.index],
-				...action.payload.newRecipe,
-			};
+			case RecipesActions.UPDATE_RECIPE:
+				const updatedRecipe = {
+					...state.recipes[action.payload.index],
+					...action.payload.newRecipe,
+				};
 
-			const updatedRecipes = [
-				...state.recipes,
-			]
-			updatedRecipes[action.payload.index] = updatedRecipe;
+				const updatedRecipes = [...state.recipes];
+				updatedRecipes[action.payload.index] = updatedRecipe;
 
-			return {
-				...state,
-				recipes: updatedRecipes,
-			};
+				return {
+					...state,
+					recipes: updatedRecipes,
+				};
 
-		case RecipesActions.DELETE_RECIPE:
-			return {
-				...state,
-				recipes: state.recipes.filter((recipe,index) => {
-					return index !== action.payload
-				})
-			}
-		default:
-			return state;
+			case RecipesActions.DELETE_RECIPE:
+				return {
+					...state,
+					recipes: state.recipes.filter((recipe, index) => {
+						return index !== action.payload;
+					}),
+				};
+			default:
+				return state;
+		}
+	} catch (e) {
+		throw e;
 	}
 }
